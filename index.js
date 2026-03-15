5db157262291c2c970b7bb92c125901459c37e92
TziviRotbard <tzivi65@gmail.com>
2026-03-15 02:22:00 +0200

    fix: change package import

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import renderApi from '@api/render-api';
 
+const express = require('express');
+const sdk = require('api')('@render-api/v1.0#dnzkv1ulkw9q3j6');
+const app = express();
+
 renderApi.auth('rnd_6LsipuKq7SwwfyW0gxtz4k8bnapn');
 renderApi.listServices({includePreviews: 'true', limit: '20'})
   .then(({ data }) => console.log(data))
